1c7066f2b0d5f98a94bca320126dfe2f0d58a7c4
alexanderh <alexanderh@brightinfo.com>
2017-06-03 12:45:18 +0300

    added Prototype patters

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFigure figure = new Rectangle(30, 40);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            figure = new Circle(30);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            Console.Read();
+        }
+    }
+
+    interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
+            //также использую функционал .NET можно было написать другую реализаци
+            //return this.MemberwiseClone() as IFigure;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямоугольник длиной {0} и шириной {1}", height, width);
+        }
+    }
+
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радиусом {0}", radius);
+        }
+    }
+}
